ade9e21549c865b36aaec9a96ebf2ec781d37d66
hadikp <hp20001025@gmail.com>
2021-11-30 19:34:54 +0100

    day01 start

---
+++ week08-lab-tasks/src/main/java/day01/NumberSequence.java
@@ -0,0 +1,13 @@
+package day01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberSequence {
+
+    private List<Integer> intList = new ArrayList<>();
+
+    public NumberSequence(List<Integer> intList) {
+        this.intList = intList;
+    }
+}
